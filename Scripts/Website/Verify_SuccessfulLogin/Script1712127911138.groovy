3427350582b85e11842428389adcb874f3f5009c
maryovos <maryosina16@gmail.com>
2024-06-14 09:18:20 +0700

    update test case

--- Scripts/Website/Verify_SuccessfulLogin/Script1712127911138.groovy
+++ Scripts/Website/Verify_SuccessfulLogin/Script1712127911138.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
+WebUI.openBrowser('http://34.128.87.8:8084/login')
 
 WebUI.navigateToUrl(GlobalVariable.url_login)
 
